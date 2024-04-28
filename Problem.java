780f5b7bdb6ad57f7d7f231817f3126d2fddcedd
Ahmed313200 <145865537+Ahmed313200@users.noreply.github.com>
2024-04-29 01:26:28 +0300

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -4,21 +4,21 @@ public class ArrayOccurrenceCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Initialize array
+       
         int[] array = new int[10];
 
-        // Input values into array
+      
         System.out.println("Enter 10 values of elements:");
         for (int i = 0; i < array.length; i++) {
             System.out.print("A[" + (i + 1) + "]= ");
             array[i] = scanner.nextInt();
         }
 
-        // Input number to check occurrence
+       
         System.out.print("Enter the element that you want to check its occurrence: ");
         int numberToCheck = scanner.nextInt();
 
-        // Count occurrences
+    
         int occurrenceCount = 0;
         for (int value : array) {
             if (value == numberToCheck) {
@@ -26,10 +26,11 @@ public class ArrayOccurrenceCounter {
             }
         }
 
-        // Display result
+     
         System.out.println("The element " + numberToCheck + " is repeated " + occurrenceCount + " times");
 
-        // Close scanner
+
+        
         scanner.close();
     }
 }
