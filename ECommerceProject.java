b70602ff7653e9a6de517b4feef4fe213f665a38
Ahmed313200 <145865537+Ahmed313200@users.noreply.github.com>
2024-04-29 01:29:13 +0300

    Update ECommerceProject.java

--- ECommerceProject.java
+++ ECommerceProject.java
@@ -6,31 +6,29 @@ public class ECommerceProject {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Test case: create electronic product
+
         electronic_product smartphone = new electronic_product(1, "smartphone", 599.9f, "Samsung", 1);
 
-        // Test case: create clothing product
+
         ClothingProduct tShirt = new ClothingProduct(2, "T-shirt", 19.99f, "Medium", "Cotton");
 
-        // Test case: create book product
         Bookproduct oopBook = new Bookproduct(3, "OOP", 39.99f, "O’Reilly", "X Publications");
 
-        // Test case: create customer
+
         System.out.println("Enter your customer ID:");
         int customerId = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character
+        scanner.nextLine(); 
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
         System.out.println("Enter your address:");
         String address = scanner.nextLine();
         Customer customer = new Customer(customerId, name, address);
 
-        // Test case: create shopping cart
         System.out.println("How many products do you want to order?");
         int nProducts = scanner.nextInt();
         Cart cart = new Cart(customer.getCustomerId(), nProducts);
 
-        // Add products to the cart
+
         for (int i = 0; i < nProducts; i++) {
             System.out.println("Enter product details for product " + (i + 1) + ":");
             System.out.println("Enter product type (1: Electronic, 2: Clothing, 3: Book):");
@@ -53,13 +51,13 @@ public class ECommerceProject {
             }
         }
 
-        // Ask user if they want to place an order
+  
         System.out.println("Do you want to place an order? (yes/no)");
         String choice = scanner.next();
         if (choice.equalsIgnoreCase("yes")) {
-            // Place order
+   
             Order order = cart.placeorder();
-            // Print order information
+     
             order.printOrderInfo();
         } else {
             System.out.println("Order not placed.");
